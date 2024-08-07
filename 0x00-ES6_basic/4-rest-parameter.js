0874ff08f1c1ed1e27fdee79684449f42a1771e1
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2024-08-07 18:00:15 +0000

    update ++ to +=

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -2,7 +2,7 @@ export default function returnHowManyArguments(...args) {
   let num = 0;
   /* eslint-disable-next-line no-unused-vars */
   for (const arg of args) {
-    num++;
+    num += 1;
   }
   return num;
 }
