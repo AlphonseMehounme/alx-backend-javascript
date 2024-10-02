6870385aa27e7eb8e6870b8e106bdb15ccb10071
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-10-02 22:58:51 +0000

    update stdin function

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function () {
-  console.log('Welcome to Holberton School, what is your name?');
-  const name = process.stdin.read()
-  console.log('Your name is:', name)
-});
 
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', function () {
+  name = process.stdin.read();
+  process.stdout.write('Your name is:', name);
+});
 process.stdin.on('end', function () {
   console.log('This important software is now closing');
 });
