e97bcc59ac96a86b31ed473de80beaa6dfbc5002
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-10-05 15:30:22 +0000

    add readableStream

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 
-async function countStudents(path) {
+function countStudents(path) {
+  const readableStream = fs.createReadStream(path, { encoding: 'utf-8', });
+  readableStream.on('data', (chunk) => {
+    completeCh = chunk;
+    ch = completeCh.split('\n');
+    console.log(ch);
+  });
+}
+/*async function countStudents(path) {
   try {
     content = await fs.readFile(path, 'utf-8');
     console.log(content);
   } catch(error) {
     console.log(error);
   }
-}
+}*/
 
 module.exports = countStudents;
